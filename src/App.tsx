e9fb026da6daac42c8769876d2fed60dad54b7ca
Gábor Soltész-Kéri <solteszkerigabor@outlook.hu>
2023-11-30 16:27:46 +0100

    Components and flagsmith logic added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { useFlags, useFlagsmith } from 'flagsmith/react';
+import { IFlagsmith } from 'flagsmith/types';
+import CardComponent from './components/card/card';
+import HeroComponent from './components/hero/hero';
 
 function App() {
 
-  const flags = useFlags(['feature_1','feature_free_plan']); // only causes re-render if specified flag values / traits change
-  const flagsmith = useFlagsmith();
+  const flags = useFlags(['feature_free_plan']); // only causes re-render if specified flag values / traits change
+  const flagsmith: IFlagsmith<string, string> = useFlagsmith();
+  const [isFreePlanEnabled, setIsFreePlanEnabled] = useState<boolean>(flags.feature_free_plan.enabled);
+
+  useEffect(() => {
+    setIsFreePlanEnabled(flags.feature_free_plan.enabled)
+  }, [flags, flagsmith]);
+
   return (
-  <div className="App">
-    feature_1_value: {flags.feature_1.value}
-    feature_free_plan: {flags.feature_free_plan.enabled}
+  <div className="root">
+    <section className="bg-white dark:bg-gray-900 h-screen">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+            <HeroComponent></HeroComponent>
+            <div id="pricing-table" className="space-y-8 lg:grid lg:grid-cols-4 sm:gap-6 xl:gap-10 lg:space-y-0">
+                {isFreePlanEnabled ? <CardComponent></CardComponent> : "ASD"}
+                <CardComponent></CardComponent>
+                <CardComponent></CardComponent>
+                <CardComponent></CardComponent>
+            </div>
+        </div>
+      </section>
   </div>
   );
 }
---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,7 @@
+export default function HeroComponent() {
+    return (
+    <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Designed for business teams like yours</h2>
+        <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+    </div>)
+}
